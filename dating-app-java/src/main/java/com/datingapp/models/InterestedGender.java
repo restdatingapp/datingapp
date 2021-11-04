503c97b1d1f030e658725227b2fcda1fdb68c625
Emanuel Pires <emanuelpires@Emanuels-Air.home>
2021-11-03 21:59:52 -0700

    missed a save

--- dating-app-java/src/main/java/com/datingapp/models/InterestedGender.java
+++ dating-app-java/src/main/java/com/datingapp/models/InterestedGender.java
@@ -2,4 +2,29 @@ package com.datingapp.models;
 
 public class InterestedGender {
 
+private int id;
+private String type;
+private int userid;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getType() {
+	return type;
+}
+public void setType(String type) {
+	this.type = type;
+}
+public int getUserid() {
+	return userid;
+}
+public void setUserid(int userid) {
+	this.userid = userid;
+}
+
+
+
+	
 }
