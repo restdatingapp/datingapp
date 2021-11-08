6ed2889a432b02d4b95480ea612caf78fd3d16dc
Taylor Jones <taylor.jones@revature.net>
2021-11-08 10:59:51 -0800

    Added styling to signup page

--- dating-app-react/src/Components/Signup/Signup.tsx
+++ dating-app-react/src/Components/Signup/Signup.tsx
@@ -1,4 +1,6 @@
 import React, {useState, useEffect} from 'react';
+import restLogo from './restLogo.png';
+import './Signup.css';
 import {useDispatch, useSelector} from 'react-redux';
 import {useNavigate, Link} from 'react-router-dom';
 import ReactDOM from 'react-dom';
@@ -7,24 +9,29 @@ import { IUser } from '../../Store/types';
 export const Signup:React.FC<any> = () => {
 
 
+
     return(
         <div className='signup'>
-            <h2>Sign up for the REST Dating app!</h2>
+            <div className = 'logoDiv'>
+                <img src={restLogo} className='logo'/>
+            </div>
+            <div className = 'signupDiv'>
+            <h3>Sign up for the REST Dating app!</h3>
             <form className = 'signup-form'>
                 <label>First Name: </label>
-                <input type='text' required />
+                <input type='text' className='signupInput' placeholder='Enter first name...' required  />
                 <br />
                 <label>Last Name: </label>
-                <input type='text' required />
+                <input type='text' className='signupInput' placeholder='Enter last name...' required />
                 <br />
                 <label>Email: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
-                <input type='text' required />
+                <input type='text' className='signupInput' placeholder='Enter email...' required />
                 <br />
                 <label>Nickname:&nbsp;&nbsp;</label>
-                <input type='text' required />
+                <input type='text' className='signupInput' placeholder='Enter Nickname (username)' required />
                 <br />
                 <label>Password: &nbsp; </label>
-                <input type='password' required />
+                <input type='password' className='signupInput' placeholder='Enter password' required />
                 <br />
                 <label>Gender: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
                 <select name='genderSelector'>
@@ -45,8 +52,9 @@ export const Signup:React.FC<any> = () => {
                 <input type='checkbox' value='non-binary' />
                 <br />
                 <br />
-                <input type='submit' value='submit' />
+                <input type='submit' className='signupSubmit' value='submit' />
             </form>
+            </div>
         </div>
     )
 }
