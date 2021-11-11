f8de4333b375faac6813b7eea8c3e3c2856c1837
restdatingapp <93549192+restdatingapp@users.noreply.github.com>
2021-11-11 12:35:49 -0800

    Update UserController.java

--- dating-app-java/src/main/java/com/datingappspringboot/controller/UserController.java
+++ dating-app-java/src/main/java/com/datingappspringboot/controller/UserController.java
@@ -108,7 +108,7 @@ public class UserController {
 		 return new ResponseEntity<User>(uServ.updateUser(user), HttpStatus.ACCEPTED);
 	}
 	
-		@PostMapping(value = "/update")
+	@PostMapping(value = "/update")
 	public User updateUser(@RequestBody User u) {
 		System.out.println(u);
 		return uServ.updateUser(u);
