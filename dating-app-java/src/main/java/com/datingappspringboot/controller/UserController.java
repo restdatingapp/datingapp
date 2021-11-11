c5bf82fa32aaff43097f0df45ad1d07c84d2c871
restdatingapp <93549192+restdatingapp@users.noreply.github.com>
2021-11-11 11:28:23 -0800

    Update UserController.java

--- dating-app-java/src/main/java/com/datingappspringboot/controller/UserController.java
+++ dating-app-java/src/main/java/com/datingappspringboot/controller/UserController.java
@@ -1,19 +1,23 @@
 package com.datingappspringboot.controller;
 
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.datingappspringboot.exceptions.UserDoesNotExistException;
+import com.datingappspringboot.models.Gender;
 import com.datingappspringboot.models.User;
 import com.datingappspringboot.services.UserService;
 
